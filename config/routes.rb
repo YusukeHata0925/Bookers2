db6c1ff3decd7f7aa08aa12ac160b4690541f51a
YusukeHata0925 <kma14second@icloud.com>
2019-03-30 14:32:01 +0000

    [finalfinish]bookers2

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 Rails.application.routes.draw do
 
   root 'top#index'
+
   get 'users/:id/edit' => 'users#edit', as: 'edit_user'
   patch 'users/:id' => 'users#update', as: 'update_user'
   get 'books/:id/edit' => 'books#edit', as: 'edit_book'
   patch 'books/:id' => 'books#update', as: 'update_book'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :books, only: [:new, :create, :index, :show, :update, :destroy]
+  resources :books, only: [:new, :create, :index, :show, :update, :destroy] do
+  collection do
+    get :about
+    end
+  end
   # resources :post_images, only: [:new, :create, :index, :show, :destroy] do
   resource :favorites, only: [:create, :destroy] do
   resource :post_comments, only: [:new, :create, :index, :show, :destroy]
